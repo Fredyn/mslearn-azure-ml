377daad322c617ce6c9d2f9d398d5b88986899f6
Fredy Nolasco <Fredyn@users.noreply.github.com>
2024-05-22 20:08:19 -0600

    Update setup.sh compute size to STANDARD_A1

--- Labs/10/setup.sh
+++ Labs/10/setup.sh
@@ -29,8 +29,8 @@ az configure --defaults workspace=$WORKSPACE_NAME
 
 # Create compute instance
 echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
-az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_B1 --type ComputeInstance 
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_A1 --type ComputeInstance 
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_B1 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_A1 --max-instances 2 --type AmlCompute 
